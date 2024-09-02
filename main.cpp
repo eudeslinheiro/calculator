37301fc6d38105493166283b9c084696b7725a26
Dreamer <49738523+eudeslinheiro@users.noreply.github.com>
2024-09-02 13:28:33 +0100

    The initial files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Eudes Linheiro on 02/09/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int a, b, r;
+int main() {
+    cout<<"Enter the 1st no.: ";
+    cin>>a;
+    cout<<"Enter the 2nd no.: ";
+    cin>>b;
+    r=a+b;
+    cout<<"The result of the sum is: "<<r<<endl;
+    return 0;
+}
