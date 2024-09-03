163ba973e2b4e3c2a0ff3df2bb080dd59693f95e
Dreamer <49738523+eudeslinheiro@users.noreply.github.com>
2024-09-03 09:24:11 +0100

    Update main.cpp
    Added more functionality/ Operation.

--- main.cpp
+++ main.cpp
@@ -8,14 +8,50 @@
 #include <iostream>
 using namespace std;
 
-int a, b, r;
+int a, b, r, choice;
 int main() {
-    cout<<"Dreamer's Calculator V1.0<<endl;
-    cout<<"Enter the 1st no.: ";
-    cin>>a;
-    cout<<"Enter the 2nd no.: ";
-    cin>>b;
-    r=a+b;
-    cout<<"The result of the sum is: "<<r<<endl;
+    cout<<"Dreamer's Calculator V1.1"<<endl;
+    cout<<"Basic Operations \n1) Addition \n2) Subtraction \n3) Multiplication \n4) Division \nEnter your choice: ";
+    cin>>choice;
+    switch(choice){
+        case 1:
+            cout<<"Enter the 1st no.: ";
+            cin>>a;
+            cout<<"Enter the 2nd no.: ";
+            cin>>b;
+            r=a+b;
+            cout<<"The result is: "<<r<<endl;
+            break;
+            
+        case 2:
+            cout<<"Enter the 1st no.: ";
+            cin>>a;
+            cout<<"Enter the 2nd no.: ";
+            cin>>b;
+            r=a-b;
+            cout<<"The result is: "<<r<<endl;
+            break;
+            
+        case 3:
+            cout<<"Enter the 1st no.: ";
+            cin>>a;
+            cout<<"Enter the 2nd no.: ";
+            cin>>b;
+            r=a*b;
+            cout<<"The result is: "<<r<<endl;
+            
+        case 4:
+            cout<<"Enter the 1st no.: ";
+            cin>>a;
+            cout<<"Enter the 2nd no.: ";
+            cin>>b;
+            r=a/b;
+            cout<<"The result is: "<<r<<endl;
+            
+        default:
+            cout<<"Invalid choice"<<endl;
+            break;
+    }
     return 0;
 }
+
