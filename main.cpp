29af833fa786753c4f1a0ca0e8964ba361b62856
Dreamer <49738523+eudeslinheiro@users.noreply.github.com>
2024-12-27 15:00:16 +0100

    Update main.cpp
    Added a struct to store the data

--- main.cpp
+++ main.cpp
@@ -5,48 +5,58 @@
 //  Created by Eudes Linheiro on 02/09/24.
 //
 
+
+
 #include <iostream>
 using namespace std;
 
-int a, b, r, choice;
+struct{
+  int firstNo, secondNo, result;
+}data;
+
 int main() {
+    int choice;
+
     cout<<"Dreamer's Calculator V1.1"<<endl;
     cout<<"Basic Operations \n1) Addition \n2) Subtraction \n3) Multiplication \n4) Division \nEnter your choice: ";
     cin>>choice;
+    
     switch(choice){
         case 1:
             cout<<"Enter the 1st no.: ";
-            cin>>a;
+            cin>>data.firstNo;
             cout<<"Enter the 2nd no.: ";
-            cin>>b;
-            r=a+b;
-            cout<<"The result is: "<<r<<endl;
+            cin>>data.secondNo;
+            data.result=data.firstNo + data.secondNo;
+            cout<<"The result is: "<<data.result<<endl;
             break;
             
         case 2:
             cout<<"Enter the 1st no.: ";
-            cin>>a;
+            cin>>data.firstNo;
             cout<<"Enter the 2nd no.: ";
-            cin>>b;
-            r=a-b;
-            cout<<"The result is: "<<r<<endl;
+            cin>>data.secondNo;
+            data.result=data.firstNo - data.secondNo;
+            cout<<"The result is: "<<data.result<<endl;
             break;
             
         case 3:
             cout<<"Enter the 1st no.: ";
-            cin>>a;
+            cin>>data.firstNo;
             cout<<"Enter the 2nd no.: ";
-            cin>>b;
-            r=a*b;
-            cout<<"The result is: "<<r<<endl;
+            cin>>data.secondNo;
+            data.result=data.firstNo * data.secondNo;
+            cout<<"The result is: "<<data.result<<endl;
+            break;
             
         case 4:
             cout<<"Enter the 1st no.: ";
-            cin>>a;
+            cin>>data.firstNo;
             cout<<"Enter the 2nd no.: ";
-            cin>>b;
-            r=a/b;
-            cout<<"The result is: "<<r<<endl;
+            cin>>data.secondNo;
+            data.result=data.firstNo / data.secondNo;
+            cout<<"The result is: "<<data.result<<endl;
+            break;
             
         default:
             cout<<"Invalid choice"<<endl;
