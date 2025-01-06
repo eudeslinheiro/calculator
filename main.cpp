49065b1cb3e76ad036747851c2d7bb0e57208561
Dreamer <49738523+eudeslinheiro@users.noreply.github.com>
2025-01-06 13:08:40 +0100

    Update main.cpp
    Simplifying the code

--- main.cpp
+++ main.cpp
@@ -6,62 +6,37 @@
 //
 
 
-
 #include <iostream>
 using namespace std;
 
-struct{
-  int firstNo, secondNo, result;
-}data;
-
 int main() {
-    int choice;
-
-    cout<<"Dreamer's Calculator V1.1"<<endl;
-    cout<<"Basic Operations \n1) Addition \n2) Subtraction \n3) Multiplication \n4) Division \nEnter your choice: ";
-    cin>>choice;
     
+    int firstNo, secondNo, result, choice;
+    cout<<"Enter The First No.: ";
+    cin>>firstNo;
+    cout<<"Enter The Second No.: ";
+    cin>>secondNo;
+    cout<<"Which Mathematical Operation Would You Like To Perform? \n1) Addition\n2) Subtraction\n3) Division\n4) Multiplication\nYour Choice: ";
+    cin>>choice;
     switch(choice){
         case 1:
-            cout<<"Enter the 1st no.: ";
-            cin>>data.firstNo;
-            cout<<"Enter the 2nd no.: ";
-            cin>>data.secondNo;
-            data.result=data.firstNo + data.secondNo;
-            cout<<"The result is: "<<data.result<<endl;
+            result=firstNo+secondNo;
+            cout<<"The Result Of The Addiction is: "<<result<<endl;
             break;
-            
         case 2:
-            cout<<"Enter the 1st no.: ";
-            cin>>data.firstNo;
-            cout<<"Enter the 2nd no.: ";
-            cin>>data.secondNo;
-            data.result=data.firstNo - data.secondNo;
-            cout<<"The result is: "<<data.result<<endl;
+            result=firstNo-secondNo;
+            cout<<"The Result Of The Subtraction is: "<<result<<endl;
             break;
-            
         case 3:
-            cout<<"Enter the 1st no.: ";
-            cin>>data.firstNo;
-            cout<<"Enter the 2nd no.: ";
-            cin>>data.secondNo;
-            data.result=data.firstNo * data.secondNo;
-            cout<<"The result is: "<<data.result<<endl;
+            result=firstNo/secondNo;
+            cout<<"The Result Of The Division is: "<<result<<endl;
             break;
-            
         case 4:
-            cout<<"Enter the 1st no.: ";
-            cin>>data.firstNo;
-            cout<<"Enter the 2nd no.: ";
-            cin>>data.secondNo;
-            data.result=data.firstNo / data.secondNo;
-            cout<<"The result is: "<<data.result<<endl;
+            result=firstNo*secondNo;
+            cout<<"The Result Of The Multiplication is: "<<result<<endl;
             break;
-            
         default:
-            cout<<"Invalid choice"<<endl;
-            break;
+            cout<<"Invalid Choice, Exiting..."<<endl;
     }
     return 0;
 }
-
