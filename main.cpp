31279abfdca63b58da1e39afcc6f612e9e685c87
Eudes <49738523+eudeslinheiro@users.noreply.github.com>
2025-01-17 09:21:05 +0100

    Update main.cpp
    Added a new functionality to convert decimal no. to binary no.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 //
 
 #include <iostream>
-#include <string>
+#include<string>
+
 using namespace std;
 
 class getDetails {
@@ -20,47 +21,67 @@ class getDetails {
     }
 };
 
+class specialOperation{
+   public:
+    int decimalNo;
+    string binaryNo;
+    string decimalToBinary(int n){
+        string binary = "";
+        while (n > 0){
+            binary = to_string(n % 2) + binary;
+            n /= 2;
+        }
+        return binary;
+    }
+};
+
 int main() {
     string choice;
     getDetails obj;
-    
+    specialOperation ob;
     cout<<"\t\tDreamer's On Calculator v.1.01! \n\n"<<endl;
     do{
-        cout<<"Enter the operator (+, -, /, * or write exit to close the application) to perform the calculations: ";
+        cout<<"Enter the operator (+, -, /, * or write exit to close the application) to perform the calculations or ( c ) to convert Decimal to Binary: ";
         cin>>choice;
         if(choice == "exit"){
-           cout<<"\nThanks for using the App!\n"<<endl;
-           break;
+            cout<<"\nThanks for using the App!\n"<<endl;
+            break;
+        } else if (choice == "c"){
+            cout<<"Enter a decimal No.: ";
+            cin>>ob.decimalNo;
+            ob.binaryNo = ob.decimalToBinary(ob.decimalNo);
+            cout<<"\nThe Decimal No.: "<<ob.decimalNo<<" = "<<ob.binaryNo<<" in Binary"<<endl;
+            break;
         }
-            switch(choice[0]){
-                case '+':
-                    obj.details();
-                    obj.result = obj.firstNo + obj.secondNo;
-                    cout<<"\nThe Result is "<<obj.result<<endl;
-                    break;
-                case '-':
-                    obj.details();
-                    obj.result = obj.firstNo - obj.secondNo;
-                    cout<<"\nThe Result is "<<obj.result<<endl;
-                    break;
-                case '/':
-                    obj.details();
-                    if(obj.secondNo == 0){
-                        cout<<"\nError: Cannot divide this number by zero!\n"<<endl;
+        else{
+            obj.details();
+                switch(choice[0]){
+                    case '+':
+                        obj.result = obj.firstNo + obj.secondNo;
+                        cout<<"\nThe Result is "<<obj.result<<endl;
+                        break;
+                    case '-':
+                        obj.result = obj.firstNo - obj.secondNo;
+                        cout<<"\nThe Result is "<<obj.result<<endl;
+                        break;
+                    case '/':
+                        if(obj.secondNo == 0){
+                            cout<<"\nError: Cannot divide this number by zero!\n"<<endl;
+                            continue;
+                        }
+                        obj.result = obj.firstNo / obj.secondNo;
+                        cout<<"\nThe Result is "<<obj.result<<endl;
+                        break;
+                    case '*':
+                        obj.result = obj.firstNo * obj.secondNo;
+                        cout<<"\nThe Result is "<<obj.result<<endl;
+                        break;
+                    default :
+                        cout<<"\nInvalid operator. Pls Try again... \n"<<endl;
                         continue;
-                    }
-                    obj.result = obj.firstNo / obj.secondNo;
-                    cout<<"\nThe Result is "<<obj.result<<endl;
-                    break;
-                case '*':
-                    obj.details();
-                    obj.result = obj.firstNo * obj.secondNo;
-                    cout<<"\nThe Result is "<<obj.result<<endl;
-                    break;
-                default :
-                    cout<<"\nInvalid operator! please try again...\n"<<endl;
-                    continue;
-            }
+                }
+        }
     } while(true);
     return 0;
 }
+
