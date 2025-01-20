c3d88a5c56c970e80f3a8ef597ca0699e31a41a9
Eudes <49738523+eudeslinheiro@users.noreply.github.com>
2025-01-20 11:09:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,31 +40,28 @@ int main() {
     getDetails obj;
     specialOperation ob;
     cout<<"\t\tDreamer's On Calculator v.1.01! \n\n"<<endl;
+    cout<<"1) Addition\n2) Subtraction\n3) Division\n4) Multiplication\n5) Decimal to Binary conversion\n6) Exit\n\nTo perform any operation enter the sign(+, -, /, *) or write c to convert from Decimal to Binary or exit to close the Application\nYour Choice: ";
     do{
         cout<<"Enter the operator (+, -, /, * or write exit to close the application) to perform the calculations or ( c ) to convert Decimal to Binary: ";
         cin>>choice;
         if(choice == "exit"){
             cout<<"\nThanks for using the App!\n"<<endl;
             break;
-        } else if (choice == "c"){
-            cout<<"Enter a decimal No.: ";
-            cin>>ob.decimalNo;
-            ob.binaryNo = ob.decimalToBinary(ob.decimalNo);
-            cout<<"\nThe Decimal No.: "<<ob.decimalNo<<" = "<<ob.binaryNo<<" in Binary"<<endl;
-            break;
         }
         else{
-            obj.details();
                 switch(choice[0]){
                     case '+':
+                       obj.details();
                         obj.result = obj.firstNo + obj.secondNo;
                         cout<<"\nThe Result is "<<obj.result<<endl;
                         break;
                     case '-':
+                       obj.details();
                         obj.result = obj.firstNo - obj.secondNo;
                         cout<<"\nThe Result is "<<obj.result<<endl;
                         break;
                     case '/':
+                       obj.details();
                         if(obj.secondNo == 0){
                             cout<<"\nError: Cannot divide this number by zero!\n"<<endl;
                             continue;
@@ -73,8 +70,15 @@ int main() {
                         cout<<"\nThe Result is "<<obj.result<<endl;
                         break;
                     case '*':
+                        obj.details();
                         obj.result = obj.firstNo * obj.secondNo;
                         cout<<"\nThe Result is "<<obj.result<<endl;
+                        break;
+                   case 'c':
+                        out<<"Enter a decimal No.: ";
+                        cin>>ob.decimalNo;
+                        ob.binaryNo = ob.decimalToBinary(ob.decimalNo);
+                        cout<<"\nThe Decimal No. "<<ob.decimalNo<<" is = "<<ob.binaryNo<<" in Binary\n"<<endl;
                         break;
                     default :
                         cout<<"\nInvalid operator. Pls Try again... \n"<<endl;
